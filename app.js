641bc70562503de7c48029e00830b078523f8f52
almeeradil <wajihaadil2@gmail.com>
2026-08-23 20:39:41 +0500

    Rename download file from Payvibes.exe to Posvibe.exe

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 function handleDownload() {
   // Yahan apni APK file ka correct path ya direct URL dein
-  const appUrl = 'Payvibes.exe'; // Agar file root folder me hai ya hosting URL hai toh poora link dein
+  const appUrl = 'Posvibe.exe'; // Agar file root folder me hai ya hosting URL hai toh poora link dein
  
   const link = document.createElement('a');
   link.href = appUrl;
-  link.download = 'PayVibes.exe';
+  link.download = 'Posvibe.exe';
   document.body.appendChild(link);
   link.click();
   document.body.removeChild(link);
